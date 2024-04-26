fff371e11f2e57465ea5f1767b2b9f31bb4bdbf9
Ratana1234 <157865323+Ratana1234@users.noreply.github.com>
2024-04-26 13:33:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,227 @@
+
+const products=[
+    {
+        "name":"Vegetables",
+        // "price":"$19.99",
+        "img":"https://sokhakrom.com/prms/uploads/sokhakrom469.jpg",
+        "title":"ផ្កាខាត់ណា"     
+               
+    },
+    
+    {
+        "name":"Fruits",
+        // "price":"$15",
+        "img":"https://t4.ftcdn.net/jpg/06/61/36/25/360_F_661362581_5BfyIy28Xnuio2oN9kziIJ7i2GUlzE6L.jpg",
+        "title":"ផ្លែគីវី"            
+    },
+    
+    {
+        "name":"Vegetables",
+        // "price":"$9.99",
+        "img":"https://www.jiomart.com/images/product/420x420/590003662/green-capsicum-500-g-0-20201118.jpg",
+        "title":"ម្ទេសប្លោក"            
+    },
+    {
+        "name":"Fruits",
+        // "price":"$1.99",
+        "img":"https://images.squarespace-cdn.com/content/v1/54cebd23e4b02ea7650b9922/1505845287553-W7TPZPGB9MFQKASM8N8H/FullSizeRender.jpg.jpeg?format=1000w",
+        "title":"ផ្លែប៉ោម"            
+    },
+    {
+        "name":"Fruits",
+        // "price":"$4.99",
+        "img":"https://chefsmandala.com/wp-content/uploads/2018/03/Banana.jpg",
+        "title":"ផ្លែចេក"            
+    },
+    {
+        "name":"Fruits",
+        // "price":"$15",
+        "img":"https://cdn.britannica.com/99/143599-050-C3289491/Watermelon.jpg",
+        "title":"ផ្លែឪឡឹក"            
+    },
+    {
+        "name":"Vegetables",
+        // "price":"$19.99",
+        "img":"https://dailyshoppingbd.com/images/detailed/166/Carrot_800X800.jpg",
+        "title":"ការ៉ុត"     
+               
+    },
+    {
+        "name":"Fruits",
+        // "price":"$15",
+        "img":"https://img.etimg.com/thumb/width-1200,height-1200,imgsize-91748,resizemode-75,msid-97952387/magazines/panache/add-papaya-to-your-plate-the-humble-fruit-aids-weight-loss-prevents-diabetes-reduces-cholesterol.jpg",
+        "title":"ផ្លែល្ហុង"            
+    },
+    {
+        "name":"Vegetables",
+        // "price":"$9.99",
+        "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Tomato_je.jpg/1200px-Tomato_je.jpg",
+        "title":"ប៉េងបោះ"         
+    },
+    {
+        "name":"Fruits",
+        // "price":"$1.99",
+        "img":"https://5.imimg.com/data5/SELLER/Default/2022/9/OQ/WU/NW/45117192/fresh-tender-coconut-500x500.jpg",
+        "title":"ផ្លែដូង"            
+    },
+    {
+        "name":"Fruits",
+        // "price":"$4.99",
+        "img":"https://cdn.shopaccino.com/rootz/products/picture3-379742_m.jpg?v=506",
+        "title":"ផ្លែម្នាស់"    
+    },
+    
+    
+ ]
+getproducts();
+        function getproducts(){
+            // const products=[
+            //     {
+            //         "name":"Vegetables",
+            //         // "price":"$19.99",
+            //         "img":"https://sokhakrom.com/prms/uploads/sokhakrom469.jpg",
+            //         "title":"ផ្កាខាត់ណា"     
+                           
+            //     },
+            //     {
+            //         "name":"Fruids",
+            //         // "price":"$15",
+            //         "img":"https://t4.ftcdn.net/jpg/06/61/36/25/360_F_661362581_5BfyIy28Xnuio2oN9kziIJ7i2GUlzE6L.jpg",
+            //         "title":"ផ្លែគីវី"            
+            //     },
+                
+            //     {
+            //         "name":"Vegetables",
+            //         // "price":"$9.99",
+            //         "img":"https://www.jiomart.com/images/product/420x420/590003662/green-capsicum-500-g-0-20201118.jpg",
+            //         "title":"ម្ទេសប្លោក"            
+            //     },
+            //     {
+            //         "name":"Fruids",
+            //         // "price":"$1.99",
+            //         "img":"https://images.squarespace-cdn.com/content/v1/54cebd23e4b02ea7650b9922/1505845287553-W7TPZPGB9MFQKASM8N8H/FullSizeRender.jpg.jpeg?format=1000w",
+            //         "title":"ផ្លែប៉ោម"            
+            //     },
+            //     {
+            //         "name":"Fruids",
+            //         // "price":"$4.99",
+            //         "img":"https://ludicon.es/tienda/1404-medium_default/cubo-banana-fruid-series.jpg",
+            //         "title":"ផ្លែចេក"            
+            //     },
+            //     {
+            //         "name":"Fruids",
+            //         // "price":"$15",
+            //         "img":"https://t4.ftcdn.net/jpg/06/61/36/25/360_F_661362581_5BfyIy28Xnuio2oN9kziIJ7i2GUlzE6L.jpg",
+            //         "title":"ផ្លែគីវី"            
+            //     },
+            //     {
+            //         "name":"Vegetables",
+            //         // "price":"$19.99",
+            //         "img":"https://sokhakrom.com/prms/uploads/sokhakrom469.jpg",
+            //         "title":"ផ្កាខាត់ណា"     
+                           
+            //     },
+            //     {
+            //         "name":"Fruids",
+            //         // "price":"$15",
+            //         "img":"https://t4.ftcdn.net/jpg/06/61/36/25/360_F_661362581_5BfyIy28Xnuio2oN9kziIJ7i2GUlzE6L.jpg",
+            //         "title":"ផ្លែគីវី"            
+            //     },
+            //     {
+            //         "name":"Vegetables",
+            //         // "price":"$9.99",
+            //         "img":"https://www.jiomart.com/images/product/420x420/590003662/green-capsicum-500-g-0-20201118.jpg",
+            //         "title":"ម្ទេសប្លោក"            
+            //     },
+            //     {
+            //         "name":"Fruids",
+            //         // "price":"$1.99",
+            //         "img":"https://images.squarespace-cdn.com/content/v1/54cebd23e4b02ea7650b9922/1505845287553-W7TPZPGB9MFQKASM8N8H/FullSizeRender.jpg.jpeg?format=1000w",
+            //         "title":"ផ្លែប៉ោម"            
+            //     },
+            //     {
+            //         "name":"Fruids",
+            //         // "price":"$4.99",
+            //         "img":"https://ludicon.es/tienda/1404-medium_default/cubo-banana-fruid-series.jpg",
+            //         "title":"ផ្លែចេក"            
+            //     },
+                
+                
+            //  ]
+             var txt1=" ";
+             for (const pro of products) {
+                txt1+=`
+                <div class="box-in3">
+                <div class="box-in3-img">
+                    <img src="${pro.img}" alt="">
+                </div>
+                <div class="box-in3-txt">
+                    <h3>${pro.name}</h3>
+                    <p>${pro.title}</p>
+                    <button type="button" id="btn-add">Buy Now</button>
+                </div>
+            </div>
+             `
+             document.querySelector(".box-menu2").innerHTML=txt1;
+             }
+        }
+        document.getElementById("user").addEventListener("click",function(){
+            document.querySelector(".box-login").style.display="block";
+        })
+        var close=document.querySelector(".box-close");
+        close.addEventListener("click",function(){
+            document.querySelector(".box-login").style.display="none";
+        })
+        var  data=document.querySelectorAll("#btn-add")
+        var data1=document.getElementById("show-data")
+        data.forEach((el,i)=>{
+        el.addEventListener("click",function(){
+            data1.innerHTML+=`
+            <tr>
+                <td>${products[i].title}</td>
+                <td>1</td>
+                <td><img src="${products[i].img}" style="width: 80px; height: 80px; object-fit: cover;" alt=""></td>
+            </tr>
+      `
+        })
+    })
+    detail();
+    function detail(){
+        // document.querySelector(".box-pop").addEventListener("click",function(){
+        //     alert(1234);
+        // })
+        var num=1;
+        var count=document.querySelector(".box-pop")
+        var btn=document.querySelectorAll("#btn-add")
+        btn.forEach((b,i)=>{
+            // var num=1;
+            b.addEventListener("click",function(){
+            count.innerHTML=num;
+            num++;
+                })
+        })
+
+    }
+    var i=0;
+        document.querySelector(".box-pop").addEventListener("click",function(){
+            if(i==0){
+                document.querySelector(".box-tb").style.display="block";
+                i=1;
+            }else{
+                document.querySelector(".box-tb").style.display="none";
+                i=0;
+            }
+        })
+        var n=0;
+        document.querySelector("#btn-search").addEventListener("click",function(){
+            if(n==0){
+                document.getElementById("search").style.display="block";
+                n=1;
+            }else{
+                document.getElementById("search").style.display="none";
+                n=0;
+            }
+        })
+        // document.getElementById("btn-search").addEventListener("click",function(){
+        //     document.getElementById("search").style.display="block";
+        // })
